aa6a202f1fffb97169febd3dab05436338ae89e0
Mykhailo0 <mykhailo9899@gmail.com>
2024-04-18 00:56:12 +0300

    Add project files.

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../OOP3.3A/FuzzyNumberO.cpp"
+#include "../OOP3.3A/FuzzyNumber.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			FuzzyNumberO A(1, 2, 3);
+			FuzzyNumberO B(2, 3, 4);
+			FuzzyNumberO C;
+			C = A + B;
+			Assert::AreEqual(C.Get_x(), 3);
+			Assert::AreEqual(C.Get_l(), 5);
+			Assert::AreEqual(C.Get_r(), 7);
+		}
+	};
+}
